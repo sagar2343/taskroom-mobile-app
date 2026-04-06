2d5de6143d4646121174ce08dd10ca3d20f4d046
Sagar <sagar259625@gmail.com>
2026-04-07 05:18:16 +0530

    updatedd base url

--- lib/config/constant/http_constants.dart
+++ lib/config/constant/http_constants.dart
@@ -7,22 +7,22 @@ class HttpConstants {
   // static String getBaseURL() {
   //   return const String.fromEnvironment("ROOT_URL");
   // }
-  // static String baseURL = 'https://musicapp-backend-5zk7.onrender.com';
-  static String get getBaseURL {
-    if (Platform.isAndroid) {
-      // Android Emulator
-      // return 'http://10.0.2.2:3000';
-      return 'http://192.168.0.167:3000'; // wifi hotspot
-      // return 'http://192.168.1.47:3000'; // office wifi hotspot
-      // return 'http://10.239.79.88:3000'; // own hotspot
-    } else if (Platform.isIOS) {
-      // iOS Simulator
-      return 'http://localhost:3000';
-    } else {
-      // Real device / fallback
-      return 'http://192.168.0.167:3000';
-    }
-  }
+  static String getBaseURL = 'https://taskroom-backend.onrender.com';
+  // static String get getBaseURL {
+  //   if (Platform.isAndroid) {
+  //     // Android Emulator
+  //     // return 'http://10.0.2.2:3000';
+  //     return 'http://192.168.0.167:3000'; // wifi hotspot
+  //     // return 'http://192.168.1.47:3000'; // office wifi hotspot
+  //     // return 'http://10.239.79.88:3000'; // own hotspot
+  //   } else if (Platform.isIOS) {
+  //     // iOS Simulator
+  //     return 'http://localhost:3000';
+  //   } else {
+  //     // Real device / fallback
+  //     return 'http://192.168.0.167:3000';
+  //   }
+  // }
 
   static Map<String, String> getHttpHeaders([String? token]) {
     Map<String, String> headers = {};
